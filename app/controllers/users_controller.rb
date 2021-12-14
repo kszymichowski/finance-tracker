af5b094dcf6171b1ce29aa26f487fc7c4d2d1244
Konstanty Szymichowski <kostek.szymi@gmail.com>
2021-12-14 07:45:26 +0100

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
-  def my_portfolio
-    @tracked_stocks = current_user.stocks
-  end
+    def my_portfolio
+        @user = current_user
+        @tracked_stocks = current_user.stocks
+    end
 
-  def friends
-    @friends = current_user.friends
-  end
+    def friends
+        @friends = current_user.friends
+    end
+
+    def show 
+        @user = User.find(params[:id])
+        @tracked_stocks = @user.stocks
+    end
 
     def search
         if params[:friend].present?
